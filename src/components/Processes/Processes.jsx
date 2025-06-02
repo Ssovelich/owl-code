953f7e0e862cb7669bd24ba3a1ea74e74cce527d
Stanislav <ssovelich@gmail.com>
2025-06-02 19:30:52 +0300

    added arrows

--- src/components/Processes/Processes.jsx
+++ src/components/Processes/Processes.jsx
@@ -1,4 +1,5 @@
 import styles from "./Processes.module.css";
+import { FaArrowRightLong } from "react-icons/fa6";
 import { useTranslation } from "react-i18next";
 
 const Processes = () => {
@@ -11,12 +12,27 @@ const Processes = () => {
         <h2 className={styles.title}>{t("common:processes_title")}</h2>
 
         <ul className={styles.wrapIcons}>
-          {steps.map((step) => (
-            <li key={step.id} className={styles.item}>
-              <svg className={styles.icon} aria-hidden="true">
-                <use href={`/sprite.svg#icon-${step.id}`} />
-              </svg>
-              <span className={styles.text}>{step.label}</span>
+          {steps.map((step, index) => (
+            <li key={step.id} className={styles.itemGroup}>
+              <div className={styles.item}>
+                <svg className={styles.icon} aria-hidden="true">
+                  <use href={`/sprite.svg#icon-${step.id}`} />
+                </svg>
+                <span className={styles.text}>{step.label}</span>
+              </div>
+
+              {/* Стрілка між іконками (не остання) */}
+              {index !== 2 && index !== 5 && (
+                <div className={styles.arrowWrap}>
+                  <FaArrowRightLong className={styles.arrow} />
+                </div>
+              )}
+              {/* Додаткова стрілка на десктопі між 3 і 4 (index === 2) */}
+              {index === 2 && (
+                <div className={`${styles.arrowWrap} ${styles.desktopOnly}`}>
+                  <FaArrowRightLong className={styles.arrow} />
+                </div>
+              )}
             </li>
           ))}
         </ul>
